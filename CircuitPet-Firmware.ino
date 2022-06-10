c55d24735708822c24fa594475c3e128c7a07b7b
subo <gamedevsubo@gmail.com>
2022-06-10 16:04:33 +0200

    Set CMake and .ino file

--- CircuitPet-Firmware.ino
+++ CircuitPet-Firmware.ino
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <Chatter.h>
+#include <CircuitOS.h>
+#include <SPIFFS.h>
+Display* display;
+Sprite* baseSprite;
 
 void setup(){
+	Serial.begin(115200);
+	Chatter.begin();
+	Input* input = Chatter.getInput();
+
+	display = Chatter.getDisplay();
+	baseSprite = display->getBaseSprite();
+
+	baseSprite->clear(TFT_WHITE);
+
+	//TESTING
+
+	display->commit();
 
 }
 
 void loop(){
-
+	LoopManager::loop();
 }
+
+
