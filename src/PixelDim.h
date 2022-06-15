8e801c7b2d536ba225e604a784cdb4d035bfebb7
subo <gamedevsubo@gmail.com>
2022-06-15 15:56:09 +0200

    Add PixelDim.h for further class implementations

---
+++ src/PixelDim.h
@@ -0,0 +1,8 @@
+#ifndef CIRCUITPET_FIRMWARE_PIXELDIM_H
+#define CIRCUITPET_FIRMWARE_PIXELDIM_H
+#include <Arduino.h>
+#include <vec2.hpp>
+
+typedef glm::vec<2, int16_t> PixelDim;
+
+#endif //CIRCUITPET_FIRMWARE_PIXELDIM_H
