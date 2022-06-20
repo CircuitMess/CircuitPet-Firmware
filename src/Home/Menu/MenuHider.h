fd07ab7a3517165daecdf6df231ca1e07ce7dc2c
subo <gamedevsubo@gmail.com>
2022-06-20 13:10:32 +0200

    Add float transition to MenuHider

--- src/Home/Menu/MenuHider.h
+++ src/Home/Menu/MenuHider.h
@@ -16,7 +16,7 @@ private:
 
 	const uint32_t duration = 300000; //micros
 	const uint8_t deltaY = 64;
-	unsigned long startTime;
+	float transition = 1.0f; //0 to a 1 real quick
 
 	const uint32_t inactivityTimeout = 30000000; //micros
 	uint32_t inactivityCount = 0;
