50d40f7867c6d94c9e0ba73ff323aca32135ebe7
subo <gamedevsubo@gmail.com>
2022-06-23 10:57:53 +0200

    Remove PixelDim

--- src/PixelDim.h
+++
@@ -1,8 +0,0 @@
-#ifndef CIRCUITPET_FIRMWARE_PIXELDIM_H
-#define CIRCUITPET_FIRMWARE_PIXELDIM_H
-#include <Arduino.h>
-#include <vec2.hpp>
-
-typedef glm::vec<2, int16_t> PixelDim;
-
-#endif //CIRCUITPET_FIRMWARE_PIXELDIM_H
