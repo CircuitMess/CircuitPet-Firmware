154a4854e70314e6d8179d4f3fee9f69b24721f8
Emil Gajšak <emil.gajsak@gmail.com>
2022-06-23 18:15:36 +0200

    Example game in .ino file, WIP

--- CircuitPet-Firmware.ino
+++ CircuitPet-Firmware.ino
@@ -1,4 +1,42 @@
 #include <Arduino.h>
+#include "src/GameEngine/Game.h"
+#include <Loop/LoopListener.h>
+#include "src/GameEngine/Rendering/RenderSystem.h"
+#include "src/GameEngine/Collision/CollisionSystem.h"
+#include "src/GameEngine/Collision/SquareCC.h"
+#include "src/GameEngine/Rendering/AnimRC.h"
+#include <Loop/LoopManager.h>
+
+class MyGame : public Game, private LoopListener {
+public:
+	MyGame() : Game("/", {{ "big1.gif", { 8, 4 }, true },
+						  { "big2.gif", { 8, 4 }, true }}),
+						  renderer(){
+	}
+
+	void loop(uint micros) override{
+
+	}
+protected:
+	void onLoad() override{
+		Serial.println("loaded");
+	}
+
+	void onStart() override{
+
+		addObject(std::make_shared<GameObject>(std::unique_ptr<AnimRC>(new AnimRC(getFile("big1.gif"))),
+		        std::unique_ptr<SquareCC>(new SquareCC({8, 9}))));
+		LoopManager::addListener(this);
+	}
+
+	void onStop() override{
+		LoopManager::removeListener(this);
+	}
+
+private:
+	RenderSystem renderer;
+	CollisionSystem collisions;
+};
 
 void setup(){
 
