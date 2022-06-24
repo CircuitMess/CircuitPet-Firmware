0cda932990376faa61dd1a65abeb9d0399b2bdf2
subo <gamedevsubo@gmail.com>
2022-06-24 10:59:37 +0200

    Change OSSprite class to fit the given sprites

--- src/Home/Sprites/OSSprite.cpp
+++ src/Home/Sprites/OSSprite.cpp
@@ -3,15 +3,18 @@
 #include <SPIFFS.h>
 
 
-OSSprite::OSSprite(Sprite* parent, uint8_t level) : sprite(parent, width, height){
+OSSprite::OSSprite(Sprite* parent, uint8_t level) : sprite(parent, osWidth+logoWidth+1, logoHeight){
 	setLevel(level);
 }
 
 void OSSprite::setLevel(uint8_t level){
 	String path = String("/OS/Level") + level + ".raw";
 	fs::File osFile = SPIFFS.open(path);
-	if(!osFile){printf("cant' open\n");}
-	sprite.drawIcon(osFile, 0, 0, width, height);
+	fs::File logoFile = SPIFFS.open("/OS/Logo.raw");
+
+	sprite.clear(TFT_TRANSPARENT);
+	sprite.drawIcon(logoFile, 0, 0, logoWidth, logoHeight);
+	sprite.drawIcon(osFile, logoWidth + 1, 2, osWidth, osHeight);
 }
 
 void OSSprite::setPos(uint8_t x, uint8_t y){
