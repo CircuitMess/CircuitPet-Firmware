0950834b4e2138994e7853c3fb451e8745850021
subo <gamedevsubo@gmail.com>
2022-07-01 06:38:27 +0200

    Add OilCan Class

---
+++ data/Games/Game1/OilCan.cpp
@@ -0,0 +1,2 @@
+
+#include "OilCan.h"
---
+++ data/Games/Game1/OilCan.h
@@ -0,0 +1,12 @@
+
+#ifndef CIRCUITPET_FIRMWARE_OILCAN_H
+#define CIRCUITPET_FIRMWARE_OILCAN_H
+#include <Display/Sprite.h>
+
+class OilCan {
+public:
+	OilCan(Sprite* sprite);
+};
+
+
+#endif //CIRCUITPET_FIRMWARE_OILCAN_H
