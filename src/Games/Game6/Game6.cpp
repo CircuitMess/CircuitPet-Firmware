fe6ea9fc79680b3aef0d645f20707698a4df4b3c
Emil Gajšak <emil.gajsak@gmail.com>
2022-07-05 13:02:46 +0200

    Added asteroids with on-screen wrapping, collision with bullets

--- src/Games/Game6/Game6.cpp
+++ src/Games/Game6/Game6.cpp
@@ -1,16 +1,29 @@
 #include <Pins.hpp>
 #include "Game6.h"
-#include "../../GameEngine/Rendering/AnimRC.h"
 #include "../../GameEngine/Rendering/StaticRC.h"
 #include "../../GameEngine/Collision/RectCC.h"
 #include "../../GameEngine/Collision/CircleCC.h"
 #include "../../GameEngine/Rendering/SpriteRC.h"
 #include <Input/Input.h>
 
-Game6::Game6() : Game("", {
-		{ "/Bg/Level6.raw",       {}, true },
-		{ "/MenuIcons/Icon1.raw", {}, true }}){
+//compiler says declarations are required, not redundant
+constexpr std::array<float, 3> Game6::asteroidSpeed;
+constexpr std::array<float, 3> Game6::asteroidRadius;
 
+Game6::Game6() : wrapWalls({ .top =  { nullptr, std::make_unique<RectCC>(glm::vec2 { wrapWallsSize.x, 100 }) },
+							 .bot =  { nullptr, std::make_unique<RectCC>(glm::vec2 { wrapWallsSize.x, 100 }) },
+							 .left =  { nullptr, std::make_unique<RectCC>(glm::vec2 { 100, wrapWallsSize.y }) },
+							 .right =  { nullptr, std::make_unique<RectCC>(glm::vec2 { 100, wrapWallsSize.y }) }
+						   }),
+				 Game("", {{ "/Bg/Level6.raw",       {}, true },
+						 { "/MenuIcons/Icon1.raw", {}, true },
+						   { "/MenuIcons/Icon2.raw", {}, true }
+				 }){
+
+	wrapWalls.top.setPos(glm::vec2{ 0, -100 } - (2*asteroidRadius[(uint8_t)AsteroidSize::Large] + 1));
+	wrapWalls.bot.setPos(glm::vec2{ 0, 128 } + (2*asteroidRadius[(uint8_t)AsteroidSize::Large] + 1));
+	wrapWalls.left.setPos(glm::vec2{ -100, 0 } - (2*asteroidRadius[(uint8_t)AsteroidSize::Large] + 1));
+	wrapWalls.right.setPos(glm::vec2{ 160, 0 } + (2*asteroidRadius[(uint8_t)AsteroidSize::Large] + 1));
 }
 
 void Game6::onLoad(){
@@ -38,6 +51,7 @@ void Game6::onLoop(float deltaTime){
 	}
 
 	updateBullets(deltaTime);
+	updateAsteroids(deltaTime);
 }
 
 void Game6::onRender(Sprite* canvas){
@@ -46,6 +60,10 @@ void Game6::onRender(Sprite* canvas){
 
 void Game6::onStart(){
 	Input::getInstance()->addListener(this);
+
+	createAsteroid(AsteroidSize::Large, {-(2*asteroidRadius[(uint8_t)AsteroidSize::Large]), 50});
+	createAsteroid(AsteroidSize::Medium, {160, 50});
+	createAsteroid(AsteroidSize::Small, {-(2*asteroidRadius[(uint8_t)AsteroidSize::Large]), 100});
 }
 
 void Game6::onStop(){
@@ -111,10 +129,69 @@ void Game6::shootBullet(){
 	Bullet b = { bullet, speed };
 	bulletPool.push_back(b);
 
-	//TODO - add collision with all asteroids
+	for(auto &asteroid: asteroidPool){
+		collision.addPair(*asteroid.gObj, *bullet, [this, b, asteroid](){
+			bulletPool.erase(std::remove(bulletPool.begin(), bulletPool.end(), b), bulletPool.end());
+			removeObject(b.gObj);
+
+			asteroidPool.erase(std::remove(asteroidPool.begin(), asteroidPool.end(), asteroid), asteroidPool.end());
+			removeObject(asteroid.gObj);
+		});
+	}
 
 	collision.wallsAll(*bullet, [this, b](){
 		bulletPool.erase(std::remove(bulletPool.begin(), bulletPool.end(), b), bulletPool.end());
 		removeObject(b.gObj);
 	});
+}
+
+void Game6::createAsteroid(Game6::AsteroidSize size, glm::vec2 pos){
+
+	auto spriteRC = std::make_unique<SpriteRC>(PixelDim { asteroidRadius[(uint8_t)size] * 2, asteroidRadius[(uint8_t)size] * 2 });
+	spriteRC->getSprite()->clear(TFT_TRANSPARENT);
+	spriteRC->getSprite()->fillCircle(asteroidRadius[(uint8_t)size], asteroidRadius[(uint8_t)size], asteroidRadius[(uint8_t)size], TFT_BROWN);
+
+	auto asteroid = std::make_shared<GameObject>(std::move(spriteRC),
+												 std::make_unique<CircleCC>(asteroidRadius[(uint8_t)size],
+																			glm::vec2 { asteroidRadius[(uint8_t)size], asteroidRadius[(uint8_t)size] }));
+	addObject(asteroid);
+	asteroid->setPos(pos);
+
+	float angle = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 360.0f));
+	glm::vec2 direction = { cos(M_PI * angle / 180.0), sin(M_PI * angle / 180.0) };
+	glm::vec2 speed = direction * asteroidSpeed[(uint8_t)size];
+
+	Asteroid a = { asteroid, speed, size };
+	asteroidPool.push_back(a);
+
+	for(auto &bullet: bulletPool){
+		collision.addPair(*asteroid, *bullet.gObj, [this, bullet, a](){
+			bulletPool.erase(std::remove(bulletPool.begin(), bulletPool.end(), bullet), bulletPool.end());
+			removeObject(bullet.gObj);
+
+			asteroidPool.erase(std::remove(asteroidPool.begin(), asteroidPool.end(), a), asteroidPool.end());
+			removeObject(a.gObj);
+		});
+	}
+
+
+
+	collision.addPair(*asteroid, wrapWalls.top, [asteroid](){
+		asteroid->setPos({ asteroid->getPos().x, 128.0f });
+	});
+	collision.addPair(*asteroid, wrapWalls.bot, [asteroid](){
+		asteroid->setPos({ asteroid->getPos().x, -(2 * asteroidRadius[(uint8_t)AsteroidSize::Large]) });
+	});
+	collision.addPair(*asteroid, wrapWalls.left, [asteroid](){
+		asteroid->setPos({ 160.0f, asteroid->getPos().y });
+	});
+	collision.addPair(*asteroid, wrapWalls.right, [asteroid](){
+		asteroid->setPos({ -(2 * asteroidRadius[(uint8_t)AsteroidSize::Large]), asteroid->getPos().y });
+	});
+}
+
+void Game6::updateAsteroids(float deltaTime){
+	for(auto& asteroid : asteroidPool){
+		asteroid.gObj->setPos(asteroid.gObj->getPos() + asteroid.velocity * deltaTime);
+	}
 }
--- src/Games/Game6/Game6.h
+++ src/Games/Game6/Game6.h
@@ -36,12 +36,42 @@ private:
 	void updateBullets(float deltaTime);
 	void shootBullet();
 
+
+	enum class AsteroidSize : uint8_t {
+		Small, Medium, Large
+	};
+
+	struct Asteroid {
+		std::shared_ptr<GameObject> gObj;
+		glm::vec2 velocity;
+		AsteroidSize size;
+
+		bool operator==(const Asteroid& other){
+			return (velocity == other.velocity) && (gObj == other.gObj) && (size == other.size);
+		}
+	};
+
+	constexpr static std::array<float, 3> asteroidSpeed = { 30.0f, 20.0f, 10.0f };
+	constexpr static std::array<float, 3> asteroidRadius = { 5.0f, 10.0f, 20.0f };
+	std::vector<Asteroid> asteroidPool;
+
+	void createAsteroid(AsteroidSize size, glm::vec2 pos);
+	void updateAsteroids(float deltaTime);
+
+	struct {
+		GameObject top;
+		GameObject bot;
+		GameObject left;
+		GameObject right;
+	} wrapWalls;
+	constexpr static glm::vec2 wrapWallsSize = { 160.0f + 4 * asteroidRadius[(uint8_t)AsteroidSize::Large] + 2,
+												 128.0f + 4 * asteroidRadius[(uint8_t)AsteroidSize::Large] + 2 };
+
 	void buttonPressed(uint i) override;
 	void buttonReleased(uint i) override;
 
 	bool leftHold = false;
 	bool rightHold = false;
-
 };
 
 
