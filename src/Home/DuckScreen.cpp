ce1362a635be59258957cfe26bcf6f347a7c8209
Emil Gajšak <emil.gajsak@gmail.com>
2022-08-03 15:39:17 +0200

    Added Game3 to DuckScreen

--- src/Home/DuckScreen.cpp
+++ src/Home/DuckScreen.cpp
@@ -4,6 +4,7 @@
 #include "../Games/TestGame.h"
 #include "../Games/Game6/Game6.h"
 #include "../Games/Game5.h"
+#include "../Games/Game4/Game4.h"
 #include <CircuitPet.h>
 
 DuckScreen::DuckScreen(Sprite* base) : State(), base(base), characterSprite(base, StatMan.getLevel(), StatMan.get().oilLevel < 25, Anim::General),
@@ -47,7 +48,7 @@ void DuckScreen::onStart(){
 			{ "Oily", GameImage(base, "/MenuIcons/Icon1.raw"), [pushGame](){pushGame(new TestGame());}},
 			{ "Flappy", GameImage(base, "/MenuIcons/Icon2.raw"), {} },
 			{ "Eaty", GameImage(base, "/MenuIcons/Icon3.raw"), {} },
-			{ "Jump & Duck", GameImage(base, "/MenuIcons/Icon4.raw"), {} },
+			{ "Jump & Duck", GameImage(base, "/MenuIcons/Icon4.raw"), [pushGame](){pushGame(new Game4());} },
 			{ "Disco danceoff", GameImage(base, "/MenuIcons/Icon5.raw"), [pushGame](){pushGame(new Game5());} },
 			{ "Space duck", GameImage(base, "/MenuIcons/Icon6.raw"), [pushGame](){pushGame(new Game6());}},
 	};
