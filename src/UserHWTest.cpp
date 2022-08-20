a07b0e56d2d079011cc2632f89decc2e8c05a179
Filip Budiša <filip@circuitmess.com>
2022-08-20 18:51:33 +0200

    Finish User HW test

--- src/UserHWTest.cpp
+++ src/UserHWTest.cpp
@@ -6,16 +6,25 @@
 #include <Audio/Piezo.h>
 #include "Notes.h"
 #include <SPIFFS.h>
+#include <FS/RamFile.h>
+#include <CircuitPet.h>
 
 const char* labels[3] = {"Press buttons to test them.",
 						 "You should hear a melody playing on the buzzer.\n\nPress any key to continue.",
 						 "The LED light should glow red, green and blue.\n\nPress any key to exit test."};
 
-const std::unordered_map<uint8_t, UserHWTest::KeyCoord> UserHWTest::KeyCoords = {
-		{ BTN_LEFT,  { 13, 74 }},
-		{ BTN_RIGHT, { 28, 74 }},
-		{ BTN_ENTER, { 43, 74 }},
-		{ BTN_BACK,  { 58, 74 }}};
+const std::unordered_map<uint8_t, uint8_t> UserHWTest::KeyMap = {
+		{ BTN_LEFT,  0},
+		{ BTN_RIGHT, 1},
+		{ BTN_ENTER, 2},
+		{ BTN_BACK,  3}};
+
+const std::array<PixelDim, 4> UserHWTest::KeyPos = {
+		PixelDim { 18, 79 },
+		PixelDim { 31, 81 },
+		PixelDim { 44, 81 },
+		PixelDim { 58, 79 }
+};
 
 const std::vector<UserHWTest::Note> UserHWTest::Notes = {
 		{ NOTE_C4, 400 },
@@ -28,39 +37,21 @@ const std::vector<UserHWTest::Note> UserHWTest::Notes = {
 		{ NOTE_C4, 600 }
 };
 
-UserHWTest::UserHWTest(Sprite* base, std::function<void()> doneFunc) : base(base), doneFunc(doneFunc), bgSprite(base, 160, 128){
+UserHWTest::UserHWTest(Sprite* base, std::function<void()> doneFunc) : base(base), doneFunc(doneFunc){
+	for(int i = 0; i < 4; i++){
+		inputTested[i] = false;
+	}
 
-	bgSprite.drawIcon(SPIFFS.open("/Games/6/bg.raw"), 0, 0, 160, 128);
+	bg = RamFile::open(SPIFFS.open("/Games/6/bg.raw"));
+	device = RamFile::open(SPIFFS.open("/Test.raw"));
 
-
-	bgSprite.push();
 	base->setTextColor(C_RGB(182, 213, 60));
 	base->drawCenterString("Hardware Test", base->width() / 2, 1, &lgfx::fonts::Font0);
-
-/*	for(const char* text : std::vector<const char*> {
-			"Press buttons to test them.",
-			"You should hear a melody playing on the buzzer.\n\nPress any key to exit test."
-	}){
-		labels.push_back(lv_label_create(container));
-		lv_obj_t* label = labels.back();
-
-		lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
-		lv_label_set_text(label, text);
-		lv_obj_set_size(label, lv_pct(100), LV_SIZE_CONTENT);
-		lv_obj_set_style_text_font(label, &pixelbasic7, 0);
-		lv_obj_set_style_text_line_space(label, 5, 0);
-		lv_obj_set_style_text_color(label, lv_color_white(), 0);
-		lv_obj_add_flag(label, LV_OBJ_FLAG_HIDDEN);
-	}
-	lv_obj_clear_flag(labels.front(), LV_OBJ_FLAG_HIDDEN);
-
-	for(int i = 0; i < buttons.size(); i++){
-		buttons[i] = false;
-	}*/
 }
 
 void UserHWTest::onStart(){
 	Input::getInstance()->addListener(this);
+	draw();
 }
 
 void UserHWTest::onStop(){
@@ -69,10 +60,77 @@ void UserHWTest::onStop(){
 	Piezo.noTone();
 }
 
-void UserHWTest::buttonPressed(uint i){
-	if(stage == 1){
-		onStop();
+void UserHWTest::draw(){
+	base->drawIcon(bg, 0, 0, 160, 128);
+	base->drawIcon(device, 80, 20, 78, 89, 1, TFT_TRANSPARENT);
 
+	base->setTextFont(0);
+	base->setTextColor(C_RGB(182, 213, 60));
+	base->setTextSize(0);
+	base->setCursor(42, 2);
+	base->print("Hardware Test");
+
+	for(int i = 0; i < 4; i++){
+		if(inputTested[i]){
+			base->fillCircle(KeyPos[i].x + 80, KeyPos[i].y + 20, 5, TFT_GREEN);
+		}
+	}
+
+	Sprite textSprite(base, 80, 120);
+	textSprite.setPos(3, 26);
+	textSprite.clear(TFT_TRANSPARENT);
+
+	textSprite.setTextFont(0);
+	textSprite.setTextSize(0);
+	textSprite.setTextColor(TFT_WHITE);
+
+	if(stage == 0){
+		textSprite.print("Press the\n");
+		textSprite.print("buttons to\n");
+		textSprite.print("test them.");
+	}else if(stage == 1){
+		textSprite.print("The RGB LED\n");
+		textSprite.print("should light\n");
+		textSprite.print("up red,\n");
+		textSprite.print("green, and\n");
+		textSprite.print("blue.\n\n");
+		textSprite.print("Press any\n");
+		textSprite.print("key to\n");
+		textSprite.print("continue.");
+	}else if(stage == 2){
+		textSprite.print("You should\n");
+		textSprite.print("hear a\n");
+		textSprite.print("melody\n");
+		textSprite.print("playing.\n\n");
+		textSprite.print("Press any\n");
+		textSprite.print("key if\n");
+		textSprite.print("everything\n");
+		textSprite.print("is OK.");
+	}
+
+	textSprite.push();
+}
+
+void UserHWTest::buttonPressed(uint i){
+	if(stage == 0){
+		auto pair = KeyMap.find(i);
+		if(pair == KeyMap.end()) return;
+		inputTested[pair->second] = true;
+		draw();
+
+		int tested = 0;
+		for(int i = 0; i < 4; i++){
+			tested += inputTested[i];
+		}
+		if(tested == 4){
+			stage2();
+			draw();
+		}
+	}else if(stage == 1){
+		RGB.setColor(Pixel::Black);
+		stage3();
+		draw();
+	}else if(stage == 2){
 		if(doneFunc){
 			stop();
 			auto func = doneFunc;
@@ -81,47 +139,38 @@ void UserHWTest::buttonPressed(uint i){
 		}else{
 			pop();
 		}
-		return;
-	}
-
-	if(stage != 0) return;
-
-	if(i >= buttons.size()) return;
-	if(buttons[i]) return;
-
-	auto pair = KeyCoords.find(i);
-	if(pair == KeyCoords.end()) return;
-
-	KeyCoord coord = pair->second;
-
-	buttons[i] = true;
-	buttonCount++;
-
-	base->fillCircle(coord.x - 3, coord.y - 3, 7, TFT_GREEN);
-
-	if(buttonCount == 4){
-		startBuzz();
 	}
 }
 
-void UserHWTest::startBuzz(){
-	if(stage == 1) return;
-
+void UserHWTest::stage2(){
 	stage = 1;
-//	lv_obj_add_flag(labels[0], LV_OBJ_FLAG_HIDDEN);
-//	lv_obj_clear_flag(labels[1], LV_OBJ_FLAG_HIDDEN);
-
+	colorTime = millis();
+	RGB.setColor(Pixel::Red);
 	LoopManager::addListener(this);
+}
+
+void UserHWTest::stage3(){
+	stage = 2;
+
 	noteIndex = 0;
 	noteTime = millis();
 	Piezo.tone(Notes[noteIndex].freq);
 }
 
 void UserHWTest::loop(uint micros){
-	if(millis() - noteTime < Notes[noteIndex].duration) return;
+	if(stage == 1){
+		if(millis() - colorTime <= 500) return;
+		colorTime = millis();
+		colorIndex = (colorIndex + 1) % 3;
 
-	noteIndex = (noteIndex + 1) % Notes.size();
-	noteTime = millis();
-	Piezo.tone(Notes[noteIndex].freq);
+		if(colorIndex == 0) RGB.setColor(Pixel::Red);
+		else if(colorIndex == 1) RGB.setColor(Pixel::Green);
+		else if(colorIndex == 2) RGB.setColor(Pixel::Blue);
+	}if(stage == 2){
+		if(millis() - noteTime < Notes[noteIndex].duration) return;
 
+		noteIndex = (noteIndex + 1) % Notes.size();
+		noteTime = millis();
+		Piezo.tone(Notes[noteIndex].freq);
+	}
 }
--- src/UserHWTest.h
+++ src/UserHWTest.h
@@ -6,11 +6,12 @@
 #include <unordered_map>
 #include <Loop/LoopListener.h>
 #include "State.h"
+#include "GameEngine/PixelDim.hpp"
 #include <Display/Sprite.h>
 
 class UserHWTest : public State, private InputListener, public LoopListener {
 public:
-	UserHWTest(Sprite* base, std::function<void()> doneFunc);
+	UserHWTest(Sprite* base, std::function<void()> doneFunc = {});
 
 	void onStart() override;
 	void onStop() override;
@@ -18,12 +19,12 @@ public:
 
 private:
 	Sprite* base;
-	Sprite bgSprite;
+	fs::File bg;
+	fs::File device;
 
-	std::array<bool, 16> buttons;
-	uint8_t buttonCount = 0;
-	struct KeyCoord { uint8_t x, y; };
-	static const std::unordered_map<uint8_t, KeyCoord> KeyCoords;
+	std::array<bool, 4> inputTested;
+	static const std::unordered_map<uint8_t, uint8_t> KeyMap;
+	static const std::array<PixelDim, 4> KeyPos;
 
 	uint32_t noteTime;
 	uint8_t noteIndex;
@@ -33,9 +34,14 @@ private:
 	uint8_t stage = 0;
 	std::function<void()> doneFunc;
 
-	void buttonPressed(uint i) override;
-	void startBuzz();
+	uint8_t colorIndex = 0;
+	uint32_t colorTime = 0;
 
+	void buttonPressed(uint i) override;
+	void stage2();
+	void stage3();
+
+	void draw();
 };
 
 #endif //CIRCUITPET_FIRMWARE_USERHWTEST_H
